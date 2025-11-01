f09710a28b9f04730aeae0104a2025356ff9d0b8
abdel-oue <dryalapah@gmail.com>
2025-11-01 18:15:03 +0100

    extraction from data

--- extraction.py
+++ extraction.py
@@ -1,16 +1,43 @@
+from datetime import datetime
 import re
 
-#ligne par ligne .search 
-line ='Sep 29 00:11:20 ns sshd[2988132]: Failed password for invalid user test from 85.209.11.27 port 35198 ssh2'
-def extraction_data(url : str)-> list[tuple[str,...]]: 
-    data = []
-    pattern = r'^(\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+.+(Failed password for)? invalid user\s+(\w+)+\s+from\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+#line ='Sep 29 00:11:20 ns sshd[2988132]: Accepted password for invalid user test from 85.209.11.27 port 35198 ssh2'
+def convert_log_date(date : str) -> datetime:
+    # This funtion returns datetime object based on the date on the log file "Sep 19 00:01:00"
+    date_stripped = date.strip().split()
+    month = date_stripped[0]
+    day = date_stripped[1]
+    time = date_stripped[2]
+    time_stripped = time.split(':')
+    month_number = datetime.strptime(month, '%b').month
+    return datetime(2025, month_number, int(day), int(time_stripped[0]),int(time_stripped[1]),int(time_stripped[2]))
 
-    match = re.search(pattern,line)
+def extract_line(line : str)-> tuple: 
+    invalid_pattern = r'^(\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+.+Invalid user\s+(\w+)+\s+from\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) port (\d{1,5}) ((\w|\d)+)'
+    failed_pattern = r'^(\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+.+Failed password for invalid user\s+(\w+)+\s+from\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) port (\d{1,5}) ((\w|\d)+)'
+    accepted_pattern = r'^(\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2})\s+.+Accepted password for invalid user\s+(\w+)+\s+from\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) port (\d{1,5}) ((\w|\d)+)'
+    status = "Failed"
+    match = re.search(failed_pattern, line)
+    if not match:
+        match = re.search(invalid_pattern, line)
+    if not match:
+        match = re.search(accepted_pattern, line)
+        status = "Accepted"
     if match : 
         date = match.group(1)
-        status = match.group(2)
-        user = match.group(3)
-        ip = match.group(4)
-        data.append((date,status,user,ip))
-    return data
+        clean_date = convert_log_date(date)
+        user = match.group(2)
+        ip = match.group(3)
+        service = match.group(5)
+        return (clean_date,user,ip,status,service)
+    return ()
+
+def extract_data(url :str) -> list[tuple[str,...]]:
+    datalist = []
+    with open(url) as f:
+        data = f.readlines()
+        for line in data:
+            linedata = extract_line(line)
+            if linedata:
+                datalist.append(linedata)
+    return datalist
---
+++ main.py
@@ -0,0 +1,4 @@
+from extraction import extract_data
+
+if __name__ == '__main__':
+    print(extract_data('./data/small-auth.log'))
